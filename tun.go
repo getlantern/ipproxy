0eaaa8543a99e0c3914d7d8389208ca3e595deb4
atavism <paul@getlantern.org>
2024-02-13 16:07:06 -0800

    update Opts

---
+++ tun.go
@@ -0,0 +1,37 @@
+package ipproxy
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/xjasonlyu/tun2socks/v2/core/device"
+	"github.com/xjasonlyu/tun2socks/v2/core/device/fdbased"
+	"github.com/xjasonlyu/tun2socks/v2/core/device/tun"
+)
+
+func parseDevice(s string, mtu uint32) (device.Device, error) {
+	if !strings.Contains(s, "://") {
+		s = fmt.Sprintf("%s://%s", tun.Driver /* default driver */, s)
+	}
+
+	u, err := url.Parse(s)
+	if err != nil {
+		return nil, err
+	}
+
+	driver := strings.ToLower(u.Scheme)
+
+	switch driver {
+	case fdbased.Driver:
+		return parseFD(u, mtu)
+	case tun.Driver:
+		return tun.Open(u.Host, mtu)
+	default:
+		return nil, fmt.Errorf("unsupported driver: %s", driver)
+	}
+}
+
+func parseFD(u *url.URL, mtu uint32) (device.Device, error) {
+	return fdbased.Open(u.Host, mtu)
+}
