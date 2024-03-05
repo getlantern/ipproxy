950b795e72c40bc141e95a193c8287513b01b7a0
Adam Fisk <myles@getlantern.org>
2024-03-05 08:28:14 -0500

    More efficient pool with pointers

--- pool.go
+++ pool.go
@@ -1,22 +1,23 @@
 package ipproxy
 
 import (
-    "sync"
+	"sync"
 )
 
 const maxUDPPacketSize = 1 << 17
 
 // Buffer pool for forwarding UDP packets
-var udpBufPool = sync.Pool {
-    New: func() any {
-        return make([]byte, maxUDPPacketSize)
-    },
+var udpBufPool = sync.Pool{
+	New: func() any {
+		b := make([]byte, maxUDPPacketSize)
+		return &b
+	},
 }
 
-func acquireBuffer() []byte {
-    return udpBufPool.Get().([]byte)
+func acquireBuffer() *[]byte {
+	return udpBufPool.Get().(*[]byte)
 }
 
-func releaseBuffer(buf []byte) {
-    udpBufPool.Put(buf)
+func releaseBuffer(buf *[]byte) {
+	udpBufPool.Put(buf)
 }
